a685c97e70ef1187a3a817e6194c228db46b0134
Alison Filgueiras <alisoncf@gmail.com>
2024-05-27 19:00:02 -0300

    Inicial

---
+++ Carros/src/main/java/model/Veiculo.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Veiculo {
+    int id;
+    String modelo;
+    String placa;
+    String cor;
+    int ano;
+    
+    
+    
+    
+}
---
+++ Carros/src/main/java/util/ConectaBD.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package util;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+/**
+ *
+ * @author Administrador
+ */
+public class ConectaBD {
+    Connection con;
+    Statement stmt;
+    public static String DataSetName = "jdbc/lojainfo";
+
+    public ConectaBD() {
+    }
+    
+    public void executa(String sql) throws Exception {
+        try {
+            Context initContext = new InitialContext();
+            Context envContext = (Context) initContext.lookup("java:/comp/env");
+            DataSource ds = (DataSource) envContext.lookup(DataSetName);
+            con = ds.getConnection();
+            stmt = con.createStatement();
+
+            ResultSet rs = stmt.executeQuery(sql);
+            fecha();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            throw new Exception(e);
+        }
+    }
+    public ResultSet getResultset(String sql) throws Exception {
+        try {
+            
+            Context envContext = new InitialContext();
+            Context initContext = (Context) envContext.lookup("java:/comp/env");
+            DataSource ds = (DataSource) initContext.lookup(DataSetName);
+            con = ds.getConnection();
+            stmt = con.createStatement();
+
+            ResultSet rs = stmt.executeQuery(sql);
+            fecha();
+            return rs;
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+
+        }
+
+    }
+    
+    private void fecha() {
+        try {
+            stmt.close();
+            con.close();
+        } catch (Exception e) {
+        }
+    }
+    
+}
